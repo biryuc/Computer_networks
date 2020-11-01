e91632095483bd0147b4bf536c969e49a0f5ca14
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2020-11-01 13:36:06 +0700

    Update server.py

--- Task1/server.py
+++ Task1/server.py
@@ -1,17 +1,18 @@
 import socket
 import time
+import zmq
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('localhost', 5555))
 sock.listen(10)
-srvmsg = 'i am server!'
+srvmsg2 = 'i am server!'
 while True:
     conn, addr = sock.accept()
     data = conn.recv(1024)
     if not data:
         break
     print('received message: ' + data.decode())
-    time.sleep(2)
-    data = data + srvmsg.encode()
-    conn.send(data)
+    #time.sleep(2)
+    data = data + srvmsg2.encode()
+    conn.send(data) #send msg to client
 
 conn.close()
