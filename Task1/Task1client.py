cd35eac9a1124c58ebe18485196bad32f04b5c90
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2020-11-03 13:19:09 +0700

    Add files via upload

---
+++ Task1/Task1client.py
@@ -0,0 +1,11 @@
+import socket
+
+while True:
+    msg = input()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 9090))
+    sock.send(msg.encode())
+
+    msg = sock.recv(1024) #получаем  по 1 кб
+    print('message from server: ' + msg.decode())
+    sock.close()
---
+++ Task1/Task1server2.py
@@ -0,0 +1,19 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 5555))
+sock2.bind(('localhost', 5556))
+sock.listen(10)
+srvmsg2 = 'i am server!'
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
+    if not data:
+        break
+    print('received message: ' + data.decode())
+
+    data = data + srvmsg2.encode()
+    conn.send(data) #send msg to client
+
+#conn.close()
