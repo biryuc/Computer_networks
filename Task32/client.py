ad8a8580428d7419ee4bfd76e70ec2f39628b3d2
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2020-11-25 12:53:53 +0700

    Update client.py

--- Task32/client.py
+++ Task32/client.py
@@ -1,12 +1,11 @@
 import socket
 import threading
 import pyaudio
-import wave
 
 class Client:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        
         while 1:
             try:
                 self.target_ip = input('Enter IP address of server --> ')
@@ -18,19 +17,16 @@ class Client:
             except:
                 print("Couldn't connect to server")
 
-        chunk_size = 1024  # 512
+        chunk_size = 1024 # 512
         audio_format = pyaudio.paInt16
         channels = 1
-        rate = 44100
+        rate = 20000
 
         # initialise microphone recording
         self.p = pyaudio.PyAudio()
-        self.playing_stream = self.p.open(format=self.p.get_format_from_width(2), channels=channels, rate=rate,
-                                          output=True,
-                                          frames_per_buffer=chunk_size)
-        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
-                                            frames_per_buffer=chunk_size)
-
+        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True, frames_per_buffer=chunk_size)
+        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk_size)
+        
         print("Connected to Server")
 
         # start threads
@@ -42,16 +38,9 @@ class Client:
             try:
                 data = self.s.recv(1024)
                 self.playing_stream.write(data)
-                frames = []
-                frames.append(data)
-                wf = wave.open("server_output.wav", 'wb')
-                wf.setnchannels(1)
-                wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
-                wf.setframerate(44100)
-                wf.writeframes(b''.join(frames))
-                wf.close()
             except:
                 pass
+
 
     def send_data_to_server(self):
         while True:
@@ -60,6 +49,5 @@ class Client:
                 self.s.sendall(data)
             except:
                 pass
-
 
 client = Client()
